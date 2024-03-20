71249c51a3304bc388b641c95c5b2d4282cc22f8
Luca Lorenzon <luca.lorenzon@it.gucci.com>
2024-03-20 07:13:25 +0100

    Luca L | refactor of ch06 - compose(map(x), map(y) == (map(compose(x,y))

--- exercises/ch06/main.js
+++ exercises/ch06/main.js
@@ -19,8 +19,8 @@ require(['jquery', 'ramda'], ($, { compose, curry, map, prop }) => {
 
   const img = src => $('<img />', { src });
   const mediaUrl = compose(prop('m'), prop('media'));
-  const mediaUrls = compose(map(mediaUrl), prop('items'));
-  const images = compose(map(img), mediaUrls);
+  const mediaToImg = compose(img, mediaUrl);
+  const images = compose(map(mediaToImg), prop('items'));
 
   // -- Impure ---------------------------------------------------------
   const render = compose(Impure.setHtml('#js-main'), images);
